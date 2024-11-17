73b3dd9ae36893b48ee6bc907f06f36f86008772
Kawan sousa dos santos <92695689+kawansousa@users.noreply.github.com>
2024-11-17 17:06:59 -0300

    ss

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const connectToDatabase = require('./database/connect');
+const router = express.Router();
+const app = express();
+const userRoutes = require('./routes/user.routes');
+const lojasRoutes = require('./routes/lojas.routes');
+const auth = require('./middlewares/auth');
+
+// Usar o CORS com as opções definidas
+app.use(cors());
+
+app.use(express.json());
+app.use(router);
+
+app.use('/usuario', userRoutes);
+app.use('/lojas', auth, lojasRoutes);
+
+connectToDatabase();
+module.exports = app;

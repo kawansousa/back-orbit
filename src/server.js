fc61432d2fd1ab83787461ef690f89d126effa5f
Kawan sousa dos santos <92695689+kawansousa@users.noreply.github.com>
2025-04-12 07:55:30 -0300

    s

--- src/server.js
+++ src/server.js
@@ -3,4 +3,4 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3333;
 
-app.listen(PORT, '0.0.0.0', () => console.log(`Servidor rodando na porta ${PORT}`)); 
+app.listen(PORT, '0.0.0.0', () => console.log(`server rodando na porta ${PORT}`)); 
